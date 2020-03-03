3906e75c5f0936df19789acff3a76ee1c754a91d
Michal ŠMAHEL <admin@ceskydj.cz>
2020-03-03 23:26:07 +0100

    Some libraries have been updated

---
+++ src/Exceptions/LoadingForeignClassException.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Mammoth\Exceptions;
+
+/**
+ * Class LoadingForeignClassException
+ *
+ * @author Michal Šmahel (ceskyDJ) <admin@ceskydj.cz>
+ * @copyright (C) 2018-Současnost, Michal ŠMAHEL, Petr Zilvar
+ * @package Mammoth\Exceptions
+ */
+class LoadingForeignClassException
+{
+
+}
